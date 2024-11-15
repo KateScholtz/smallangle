a6d8826296032e7bf28ab334681290ec4557c94d
KateScholtz <171558394+KateScholtz@users.noreply.github.com>
2024-11-15 16:04:41 +0100

    Update smallangle
    Added commends and docstrings.

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,18 +3,40 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def cmd_group():
+    pass
 
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default = 0,
+)
 def sin(number):
+    """Create a list with NUMBER points between 0 and 2π and the sinus of those points.
+
+    NUMBER is the amount of points in the list.
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default = 0, 
+)
 def tan(number):
+    """Create a list with NUMBER points between 0 and 2π and the tangent of those points.
+
+    NUMBER is the amount of points in the list.
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
 
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
